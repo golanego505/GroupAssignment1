114bd0b22be2dca2049d9a9ddd2afcda903dc533
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-02-10 18:41:58 -0600

    Added members to library class: Book array and int count.

--- src/edu/mu/bookmanagementsystem/Library.java
+++ src/edu/mu/bookmanagementsystem/Library.java
@@ -1,5 +1,15 @@
 package edu.mu.bookmanagementsystem;
 
 public class Library {
-
+	
+	/**
+	 * Two Attributes:
+	 * book is an array of Book objects
+	 * count is the number of Books within book array
+	 */
+	private Book[] books = new Book[5];
+	private int count;
+	
+	
+	
 }
